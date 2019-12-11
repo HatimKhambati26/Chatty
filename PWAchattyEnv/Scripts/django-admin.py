e21a870aeb71f40fe807ff0871abc635de126f97
hatimkhambati26 <hatim.khambati26@gmail.com>
2019-12-11 15:01:57 +0530

    First Commit

---
+++ PWAchattyEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Hatim\Desktop\Snoop Project\PWA\chatty\PWAchattyEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
